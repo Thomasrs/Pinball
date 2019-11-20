850a57305843b6aa8a8e4f1ad4909f7f0faa19eb
Pedrocereja <52168131+Pedrocereja@users.noreply.github.com>
2019-11-20 16:24:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-while 1{
+#Setup
+Player = player()
+sensorInicio #Sensor de início de jogo
+sensorPontuacao #Sensor genérico de pontuação
+sensorPontuacao1
+sensorPontuacao2
+sensorPontuacao3
 
-}
+#Execução do jogo
+while True:
+  telaInicial()#Exibe tela inicial até o lançamento da bola
+  
+  
+def telaInicial():  
+  while sensorInicio=="inativo":
+    #Plotar gráficos da tela inicial
+      
