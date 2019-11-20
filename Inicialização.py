1bee53fa9a589c27189ce74c92ea4ca9cf379503
Pedrocereja <52168131+Pedrocereja@users.noreply.github.com>
2019-11-20 15:05:59 -0300

    Update Inicialização.py

--- Inicialização.py
+++ Inicialização.py
@@ -12,3 +12,21 @@ pin1.enable_reporting
 sleep(1)
 while 1:
 	if(pin1.read())
+	
+class Player:
+	
+	def __init__(self): #Atributos do player
+		self.vidas = 3 #Número de vidas
+		self.pontuacao = 0 #Pontos
+		
+	def pontuar(self, pontos): #Aumentar pontuação
+		self.pontuação += pontos
+		#Se a pontuação exceder x tocamos uma músicas ou exibimos algo na tela
+		#COCOCOCCOOMBOBREAKER
+		
+	def morrer(self): #Perder uma vida
+		self.vidas -= 1
+		if (self.vidas==0):
+			self.nomear()
+			self.__init__()
+		
