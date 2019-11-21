1eb8b083d47ae43e53d0fb4867e6835c17c05a6c
Pedrocereja <52168131+Pedrocereja@users.noreply.github.com>
2019-11-21 12:55:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #Setup
-Player = player()
-sensorInicio #Sensor de início de jogo
-sensorPontuacao #Sensor genérico de pontuação
-sensorPontuacao1
-sensorPontuacao2
-sensorPontuacao3
+player = player()
+sensor_inicio #Sensor de início de jogo
+sensor_pontuacao #Sensor genérico de pontuação
+sensor_pontuacao1
+sensor_pontuacao2
+sensor_pontuacao3
 
 #Execução do jogo
 while True:
