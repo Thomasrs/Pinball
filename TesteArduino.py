3a688fa84e22a8d1b29da29f031be782ba7cdec9
Pedrocereja <52168131+Pedrocereja@users.noreply.github.com>
2019-11-26 14:32:59 -0300

    Update TesteArduino.py

--- TesteArduino.py
+++ TesteArduino.py
@@ -7,8 +7,16 @@ sleep(5)
 print("inicio")
 it = util.Iterator(board)
 it.start()
-pin1 = board.get_pin('d:8:i')
+pin1 = board.get_pin('a:2:i')
 pin1.enable_reporting
 sleep(1)
+
+pontuacao = 0
 while 1:
-	if(pin1.read())
+	
+	if(pin1.read() > 500):
+		pontuacao += 1
+		sleep(0.05)
+		print(pontuacao)
+	
+
