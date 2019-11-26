634d6c95a59732dfefbee8fc76c5bd2ea1d8a299
Pedrocereja <52168131+Pedrocereja@users.noreply.github.com>
2019-11-26 17:24:47 -0300

    Update TesteArduino.py

--- TesteArduino.py
+++ TesteArduino.py
@@ -8,18 +8,19 @@ it.start()
 
 class Sensor:
 	def __init__(self, pin, valor):
-		self.pin = pin
+		self.pin = board.get_pin('a:{0}:i'.format(pin))
 		self.valor = valor
+		self.pin.enable_reporting
+		sleep(0.05)
 	def status(self):
-		pino = board.get_pin('a:'pin':i')
-		pin1.enable_reporting
-		if(pino.read() > 0.500):
+		if(self.pin.read() > 0.500):
 			return 1
-		else
+		else:
 			return 0
 		
-sensor = Sensor(2,500)
-while 1:
-	print(sensor.status())
-	sleep(1)
+#sensor = Sensor(2,500)
+#while 1:
+#	print(sensor.status())
+#	sleep(0.05)
 
+	
