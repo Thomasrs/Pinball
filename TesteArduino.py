b9b348002a1aa73898ec7852e0caa80ef7e7ed78
Thomasrs <49652995+Thomasrs@users.noreply.github.com>
2019-11-26 16:15:52 -0300

    Update TesteArduino.py

--- TesteArduino.py
+++ TesteArduino.py
@@ -3,20 +3,23 @@ from time import sleep
 
 board = Arduino('/dev/ttyACM0', baudrate = 250000)
 sleep(5)
-
-print("inicio")
 it = util.Iterator(board)
 it.start()
-pin1 = board.get_pin('a:2:i')
-pin1.enable_reporting
-sleep(1)
 
-pontuacao = 0
+class Sensor:
+	def __init__(self, pin, valor):
+		self.pin = pin
+		self.valor = valor
+	def status(self):
+		pino = board.get_pin('a:'pin':i')
+		pin1.enable_reporting
+		if(pino.read() > 0.500):
+			return 1
+		else
+			return 0
+		
+sensor = Sensor(2,500)
 while 1:
+	print(sensor.status())
+	sleep(1)
 
-	print(pin1.read())	
-	sleep(0.01)
-	if(pin1.read() > 0.500):
-		pontuacao += 1
-		sleep(0.05)
-		print(pontuacao)
